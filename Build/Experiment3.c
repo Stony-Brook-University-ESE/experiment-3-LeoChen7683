57bfd09ec596cb184a9322aa01ea60cb483a0abd
LeoChen7683 <leo.c.chen@stonybrook.edu>
2025-05-12 13:00:20 -0400

    done

--- Build/Experiment3.c
+++ Build/Experiment3.c
@@ -4,13 +4,15 @@ int main() {
     int n = 1000;
     int matrix[n][n];
 
+    int value = 1;
     for (int i = 0; i < n; i++) {
         for (int j = 0; j < n; j++) {
-            matrix[i][j] = i * n + j + 1;
+            matrix[i][j] = value++;
+            printf("%4d ", matrix[i][j]);
         }
     }
 
-    for (int i = n - 1; i >= 0; i--) {
+    for (int i = n - 1; i >= 0; i--) {               
         for (int j = n - 1; j >= 0; j--) {
             printf("%4d ", matrix[i][j]);
         }
